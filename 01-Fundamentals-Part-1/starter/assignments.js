f683fb9fcdbbbb35156e3fdb2b7a9736a96be20b
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-08-14 15:52:59 +0100

    Add assignments.js and update index.html to include it

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "Poland";
+let continent = "Europe";
+let population = 6000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,11 @@
  let js = "amazing";
-      if (js === "amazing") alert ("JavaScript is FUN!");
       console.log(40 + 8 + 23 - 10);
+
+
+      console.log('Jonas')
+      console.log(23);
+
+      let firstName = 'Ola';
+      console.log(firstName);
+      console.log(firstName);
+      console.log(firstName);
